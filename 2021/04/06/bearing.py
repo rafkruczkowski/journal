a5d6e40471bb3fd094d079d9a1294c4dbcfee9c4
rafkruczkowski@github.com <kruczkowski@gmail.com>
2021-04-06 13:30:35 -0700

    bearing calculator

---
+++ 2021/04/06/bearing.py
@@ -0,0 +1,73 @@
+import math
+from math import radians, cos, sin, asin, sqrt
+
+def calculate_initial_compass_bearing(pointA, pointB):
+    """
+    Calculates the bearing between two points.
+    The formulae used is the following:
+        θ = atan2(sin(Δlong).cos(lat2),
+                  cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
+    :Parameters:
+      - `pointA: The tuple representing the latitude/longitude for the
+        first point. Latitude and longitude must be in decimal degrees
+      - `pointB: The tuple representing the latitude/longitude for the
+        second point. Latitude and longitude must be in decimal degrees
+    :Returns:
+      The bearing in degrees
+    :Returns Type:
+      float
+    """
+    if (type(pointA) != tuple) or (type(pointB) != tuple):
+        raise TypeError("Only tuples are supported as arguments")
+
+    lat1 = math.radians(pointA[0])
+    lat2 = math.radians(pointB[0])
+
+    diffLong = math.radians(pointB[1] - pointA[1])
+
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+            * math.cos(lat2) * math.cos(diffLong))
+
+    initial_bearing = math.atan2(x, y)
+
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180° to + 180° which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+#pointA = (11.11, 22.22)
+#pointB = (12.11, 23.22)
+
+pointA_Lat = input("Enter A Lat: ")
+pointA_Lon = input("Enter A Lon: ")
+pointB_Lat = input("Enter B Lat: ")
+pointB_Lon = input("Enter B Lon: ")
+
+pointA = (float(pointA_Lat), float(pointA_Lon))
+pointB = (float(pointB_Lat), float(pointB_Lon))
+
+distance = haversine(float(pointA_Lon), float(pointA_Lat), float(pointB_Lon), float(pointB_Lat))
+
+resultBearing = calculate_initial_compass_bearing(pointA,pointB)
+print("Bearing:", resultBearing)
+print("Distance km:", distance)
