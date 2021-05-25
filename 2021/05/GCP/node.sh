7e99af5cfce7e5d5a8c31ed1aaca8e6b01a5346a
rafkruczkowski@github.com <kruczkowski@gmail.com>
2021-05-24 18:52:07 -0700

    GCP

---
+++ 2021/05/GCP/node.sh
@@ -0,0 +1,24 @@
+#GCP K8s Node bootstrap
+swapoff -a
+systemctl stop firewalld
+systemctl disable firewalld
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl jq tree --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y
+dnf install docker-ce --nobest -y
+systemctl start docker
+kubeadm config images pull
